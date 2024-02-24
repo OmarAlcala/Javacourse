d4a3f8b1833d587dcfa1d527ef0462697ad760aa
OmarAlcala <pedro.omar.alcala@gmail.com>
2024-02-24 20:29:40 +0100

    string function

--- ProjectoUno/src/ManupilaCadenasDos.java
+++ ProjectoUno/src/ManupilaCadenasDos.java
@@ -1,5 +1,7 @@
 public class ManupilaCadenasDos {
     public static void main(String[] args) {
-
+        String frase=" hoy es dia de aprender a programar en java";
+        String sub_string=frase.substring(0,5);
+        System.out.print(sub_string);
     }
 }
